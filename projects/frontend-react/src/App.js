9ee9933ca4979aa78c87b7d656a7431e1ef0a210
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-15 01:04:07 -0400

    feat(react): first home page

--- projects/frontend-react/src/App.js
+++ projects/frontend-react/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Joke from './components/Joke.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src="logo.png" className="logo" alt="logo" />
       </header>
+      <Joke
+        category="Category"
+        text="Un fils peut apporter le bénépère en retour"
+        author="Berdal84">
+      </Joke>
+      <footer>
+        <p >This is the React version, <a href="https://www.relativementutile.fr/jeudemots">go to the original version</a></p>
+      </footer>
     </div>
   );
 }
