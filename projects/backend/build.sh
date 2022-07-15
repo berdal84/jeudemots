9a0ee52af29f0d546e42f5438f026f4ad9db82fd
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-15 01:46:16 -0400

    fix(ci): backend build

--- projects/backend/build.sh
+++ projects/backend/build.sh
@@ -9,6 +9,12 @@ cd ${SCRIPT_DIR};
 rm -rf build || exit "Unable to delete existing build folder!"
 mkdir -p build || exit "Unable to create build folder!"
 cp -av src/ build || exit "Unable to copy PHP files to build folder!"
-cp -a conf/config.prod.php build/config.php || exit "Unable to copy config.prod.php!"
-
+ 
+CONFIG_PROD=conf/config.prod.php
+if test -f "$CONFIG_PROD"; then
+    cp -a $CONFIG_PROD build/config.php || exit "Unable to copy config.prod.php!";
+    echo "Configuration file copied!"
+else
+    echo "WARNING: no configuration file found!";
+fi
 echo "Building backend OK - output: ${SCRIPT_DIR}/build"
