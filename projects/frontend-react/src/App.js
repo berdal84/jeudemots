499558e7046497e4e16c3c41b4033f792df973b4
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-15 10:13:11 -0400

    refactor(App): store currentJoke in state

--- projects/frontend-react/src/App.js
+++ projects/frontend-react/src/App.js
@@ -1,22 +1,36 @@
+import React from 'react';
 import './App.css';
 import Joke from './components/Joke.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <img src="logo.png" className="logo" alt="logo" />
-      </header>
-      <Joke
-        category="Category"
-        text="Un fils peut apporter le bénépère en retour"
-        author="Berdal84">
-      </Joke>
-      <footer>
-        <p >This is the React version, <a href="https://www.relativementutile.fr/jeudemots">go to the original version</a></p>
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(state) {
+    super(state);
+
+    this.state = {
+      currentJoke: {
+        category: "Category",
+        text: "This is a joke text",
+        author: "Author"
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <img src="logo.png" className="logo" alt="logo" />
+        </header>
+        <Joke {...this.state.currentJoke} />
+        <footer>
+          <p >This is the React version,
+            <a href="https://www.relativementutile.fr/jeudemots">go to the original version</a>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
