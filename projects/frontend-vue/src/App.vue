6273a8edd69424a78f788073363b07b35f3f949c
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2022-07-20 17:09:05 -0400

    feat(front): new frontend using vuejs, WIP

---
+++ projects/frontend-vue/src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <div className="App">
+    <header>
+      <img src="./assets/logo.png" className="logo" alt="logo" />
+    </header>
+    <Joke
+      category="WIP"
+      text="This is a joke"
+      author="Berdal84"
+      date="20-07-2022"
+    />
+    <footer>
+      <p>This is the Vue version, go to the original 
+        <a href="https://www.relativementutile.fr/jeudemots">Angular version</a>
+      </p>
+    </footer>
+  </div>
+  
+</template>
+
+<script>
+import Joke from './components/Joke.vue'
+import './theme.css'
+
+export default {
+  name: 'App',
+  components: {
+    Joke
+  }
+}
+</script>
+
+
+<style>
+body {
+  margin: 0;
+  font-family: var(--font-family);
+  font-size: var(--font-size);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+p {
+  color: var(--text-light);
+}
+
+a {
+  color: var(--text-light);
+  transition: 0.5s;
+}
+
+a:hover {
+  color: var(--text-light);
+}
+
+.App {
+  text-align: center;
+    display: flex;
+    flex-direction: row;
+    min-height: 100vh;
+    background-color: var(--bg-light);
+    flex-wrap: nowrap;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+}
+
+.App .logo {
+  pointer-events: none;
+}
+
+.App header {
+  display: flex;
+  flex-direction: column;
+  flex: 0.2 0.2;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+
+.Joke {
+  flex: 0.6 0;
+  padding: 20px;
+}
+
+.App footer {
+  position: fixed;
+  align-items: center;
+  bottom: 0px;
+  width: 100vw;
+  background-color: var(--bg-dark);
+  opacity: 0.75
+}
+
+.App footer > p  {
+  margin: 5px;
+}
+
+.spacer {
+  flex: 30px;
+}
+
+</style>
---
+++ projects/frontend-vue/src/components/Joke.vue
@@ -0,0 +1,73 @@
+<template>
+  <div className="Joke">
+    <h1 className="category tab">{{category}}</h1>
+    <div className="text">
+      <h1 className>{{text}}</h1>
+    </div>
+    <h1 className="author tab">{{author}}</h1>
+    <h1 className="date">{{date}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Joke-View',
+  props: {
+    category: String,
+    text: String,
+    author: String,
+    date: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.Joke {
+    display: flex;
+    justify-content: center;
+    color: var(--text-light);
+    text-shadow: var(--text-shadow);
+    flex-direction: column;
+}
+
+.category {
+    font-size: 2em;
+    align-self: start;
+    border-radius: 20px 20px 0 0;
+    margin-left: 20px;
+    margin-bottom: 0;
+}
+
+div.text {
+    margin: 0;
+    padding: var(--padding-lg);
+    border-radius: 20px;
+    background-color: var(--bg-dark);
+    border: solid var(--bg-light) 5px;
+    min-height: 7.5em;
+    transition: height 1s ease-out;    
+}
+
+div.text > * {
+    font-size: 2.5em;
+    text-align: left;
+    padding: 0;
+    margin: 0;
+}
+
+.author {
+    border-radius: 0 0 20px 20px;
+    padding: var(--padding-lg);
+    font-size: 1.2em;
+    align-self: end;
+    margin-right: 20px;
+    margin-top: 0;
+}
+
+.tab {
+    padding: var(--padding-sm) var(--padding-lg) var(--padding-sm) var(--padding-lg) ;
+    background-color: var(--bg-dark);
+}
+</style>
