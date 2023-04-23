40f6e4e18421d14069800fd1e3b7c11a321b155f
Berenger Dalle-Cort <berenger@42borgata.com>
2023-04-23 18:28:10 -0400

    feat(server): update config.sample.php

--- server/src/config.sample.php
+++ server/src/config.sample.php
@@ -1,12 +1,31 @@
 <?php
 
-/* Edit this file, replace values and rename as config.prod.php */
-define('DB_HOST', 'database_host');
-define('DB_NAME', 'database_name');
-define('DB_USER', 'database_user');
-define('DB_PASS', 'database_pass');
-define('ADMIN_USER', 'admin_user');
-define('ADMIN_PASS', 'admin_pass'); // sha256
-define('ADMIN_EMAIL', 'me@domain.com'); // to get notified
-define('ACCESS_CONTROL_ALLOW_ORIGIN', 'https://jeudemots.42borgata.com');
+// When invoking the script via CLI like
+// "php -f script.php name1=value1 name2=value2",
+// this code will populate $_GET variables called
+// "name1" and "name2", so a script designed to
+// be called by a web server will work even
+// when called by CLI
+if (php_sapi_name() == "cli") {
+    for ($c = 1; $c < $argc; $c++) {
+        $param = explode("=", $argv[$c], 2);
+        $_GET[$param[0]] = $param[1]; // $_GET['name1'] = 'value1'
+        //print($param[0]."=".$param[1]);
+    }
+}
+
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+
+define('ADMIN_USER', '');
+define('ADMIN_PASS', '');
+define('ADMIN_EMAIL', '');
+define('ACCESS_CONTROL_ALLOW_ORIGIN', '');
+define('COOKIE_DOMAIN', '');
+define('DEBUG', false);
+
+if( DEBUG) ini_set('display_errors', 1);
+
 ?>
