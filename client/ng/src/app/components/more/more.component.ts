92331dceda90f5e93c6e44fd9f603f557d4728a8
berdal84 <berenger@42borgata.com>
2023-04-25 22:08:50 -0400

    refactor(MoreComponent): single file

--- client/ng/src/app/components/more/more.component.ts
+++ client/ng/src/app/components/more/more.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-more',
-  templateUrl: './more.component.html',
-  styleUrls: ['./more.component.css']
+  template: `
+    <h2>A propos...</h2>
+    <section class="indente">
+      <p>Ce site a été créé pour me former. Dans un premier temps il n'était qu'une simple page AngularJS, il a
+          depuis beaucoup évolué (avec un backend et une base de données).</p>
+      <p>L'objectif secondaire est évidemment de s'amuser en écrivant les jeux de mots...</p>
+      <p>Point de vue technique, ce site a été créé initialement avec AngularJS 1.x., puis il a été mis à jour
+          vers Angular 2+ dernièrement
+          (<a href="https://github.com/berdal84/jeudemots-ng" title="Consultez le code source sur GitHub">voir
+              code source</a>).
+      </p>
+    </section>
+  `
 })
-export class MoreComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MoreComponent {
 }
