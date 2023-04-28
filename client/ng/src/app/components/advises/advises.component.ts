e95ff910aa93d0646e1614bc69446ab5b37f6241
berdal84 <berenger@42borgata.com>
2023-04-28 02:18:00 -0400

    remove useless text

--- client/ng/src/app/components/advises/advises.component.ts
+++ client/ng/src/app/components/advises/advises.component.ts
@@ -8,11 +8,6 @@ import { Component } from '@angular/core';
           <li>Soyez vous-même.</li><br>
           <li>Ne craignez pas la moquerie.</li><br>
           <li>N'abandonnez pas au premier échec.</li><br>
-          <li>Ne vous précipitez pas, mieux vaut pas de jeu de mots qu'un jeu de mots hors contexte.</li><br>
-          <li>Ressentez la force qui est en vous.</li><br>
-          <li>Mangez 5 fruits et légumes par jour.</li><br>
-          <li>Faites du sport.</li><br>
-          <li>Ayez des rapports sexuels réguliers (très important pour le "lâcher-prise").</li><br>
       </ol>
       <h3>Conseil spécial de M. DUSS:</h3>
       <p class="indent">"Oublie que t'as aucune chance, vas-y fonce. Sur un mal entendu ça peut marcher."
