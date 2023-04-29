40b505460976caa26bb8bde5cdd52bfb21e2f7b7
Berenger Dalle-Cort <berenger@42borgata.com>
2023-04-29 00:06:18 -0400

    fix(more): angular versions

--- client/ng/src/app/components/more/more.component.ts
+++ client/ng/src/app/components/more/more.component.ts
@@ -9,12 +9,11 @@ import {Component} from '@angular/core';
           depuis beaucoup évolué (avec un backend et une base de données).</p>
       <p>L'objectif secondaire est évidemment de s'amuser en écrivant les jeux de mots...</p>
       <p>Point de vue technique, ce site a été créé initialement avec AngularJS 1.x., puis il a été mis à jour
-          vers Angular 2+ dernièrement
+          vers Angular v2 (passage en typescript) jusqu'à la v15 dernièrement.
           (<a href="https://github.com/berdal84/jeudemots-ng" title="Consultez le code source sur GitHub">voir
               code source</a>).
       </p>
     </section>
   `
 })
-export class MoreComponent {
-}
+export class MoreComponent {}
