fa2fb2efec1beb05bc874ec12f6ba35bf13639ca
berdal84 <berenger@42borgata.com>
2023-04-30 10:59:13 -0400

    fix(client/ng): advises content

--- client/ng/src/app/components/advises/advises.component.ts
+++ client/ng/src/app/components/advises/advises.component.ts
@@ -2,6 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-advises',
+  styles: [`
+    #quote {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 1rem;
+    }
+    #quote > #text {
+      flex: 3;
+    }
+    #quote > #author {
+      flex: 1;
+      padding: 0 5rem;
+    }
+  `],
   template: `
       <h3>Quelques conseils pour réussir vos blagues comme un pro :</h3>
       <ol class="indent">
@@ -9,10 +24,13 @@ import { Component } from '@angular/core';
           <li>Ne craignez pas la moquerie.</li><br>
           <li>N'abandonnez pas au premier échec.</li><br>
       </ol>
-      <h3>Conseil spécial de M. DUSS:</h3>
-      <p class="indent">"Oublie que t'as aucune chance, vas-y fonce. Sur un mal entendu ça peut marcher."
-          (<a href="https://www.youtube.com/watch?v=rIktiASrDFo">référence</a>)</p>
-      <br>
+      <h3>Conseil "Spécial"</h3>
+      <div class="indent">
+        <p id="quote">
+          <cite id="text">"Oublie que t'as aucune chance, vas-y fonce. On sait jamais, sur un mal entendu ça peut marcher."</cite>
+          <span id="author">Jean-Claude Dusse</span>
+        </p>
+      </div>
   `
 })
 export class AdvisesComponent {}
