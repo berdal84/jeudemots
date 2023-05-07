e11b667fc170aba3df61eb478287c70790b973e3
berdal84 <berenger@42borgata.com>
2023-05-07 15:25:04 -0400

    refactor(main.ts): extract app.config.ts

--- client/ng/src/main.ts
+++ client/ng/src/main.ts
@@ -2,17 +2,10 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
