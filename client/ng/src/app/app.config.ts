806f3a5841a9552dc2f00082795c4ff63aca5f73
Berenger Dalle-Cort <berenger@42borgata.com>
2023-05-11 21:16:38 -0400

    fix(client/ng): use hash location for router

--- client/ng/src/app/app.config.ts
+++ client/ng/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient()
 ]
 };
