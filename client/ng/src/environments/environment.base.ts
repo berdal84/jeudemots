90a9f804fb8ea2bb76fa338ccf5aa1c79732dd63
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-04-27 11:01:28 -0400

    feat(docker): improve docker-compose
    Add networks and container names.

--- client/ng/src/environments/environment.base.ts
+++ client/ng/src/environments/environment.base.ts
@@ -2,7 +2,7 @@ import {Environment} from './environment.model';
 
 export const environmentBase = {
   api: {
-    baseUrl: '127.0.0.1:8000',
+    baseUrl: 'http://127.0.0.1:8000',
     path: {
       auth: '/authentication.php',
       mail: '/mail.php',
